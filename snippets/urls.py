b7a848b425d31a1ce013c5920afc10a9cadd87b1
Ritz Ghever Libetario <yahshua.ecpvav@gmail.com>
2021-09-17 08:27:18 +0000

    Part 6 Routers

--- snippets/urls.py
+++ snippets/urls.py
@@ -5,32 +5,13 @@ from snippets.views import Snippet_View_Set, User_View_Set, api_root
 
 from rest_framework.urlpatterns import format_suffix_patterns
 from rest_framework import renderers
+from rest_framework.routers import DefaultRouter
 
 
-snippet_list = Snippet_View_Set.as_view({
-    'get': 'list',
-    'post': 'create'
-})
-snippet_detail = Snippet_View_Set.as_view({
-    'get': 'retrieve',
-    'put': 'update',
-    'patch': 'partial_update',
-    'delete': 'destroy'
-})
-snippet_highlight = Snippet_View_Set.as_view({
-    'get': 'highlight'
-}, renderer_classes=[renderers.StaticHTMLRenderer])
-user_list = User_View_Set.as_view({
-    'get': 'list'
-})
-user_detail = User_View_Set.as_view({
-    'get': 'retrieve'
-})
+router = DefaultRouter()
+router.register(r'snippets', views.Snippet_View_Set)
+router.register(r'users', views.User_View_Set)
 
-urlpatterns = format_suffix_patterns([
-    path('', api_root),
-    path('snippets/', snippet_list, name='snippet-list'),
-    path('snippets/<int:pk>/', snippet_detail, name='snippet-detail'),
-    path('snippets/<int:pk>/highlight/', snippet_highlight, name='snippet-highlight'),
-    path('users/', user_list, name='user-list'),
-])
+urlpatterns = [
+    path('', include(router.urls)),
+]
